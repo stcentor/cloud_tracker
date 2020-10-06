36ea9578f6c07c90a2b0bfcfeecaf2153e7c52a9
stcentor <skyler.tangneycentor@gmail.com>
2020-10-06 10:03:59 -0400

    Create test_tag.py

---
+++ test_tag.py
@@ -0,0 +1,55 @@
+
+import os
+import pytest
+from ibm_cloud_sdk_core import ApiException, read_external_sources
+from ibm_platform_services.global_tagging_v1 import *
+
+
+config_file = 'global_tagging.env'
+
+
+
+global global_tagging_service
+if os.path.exists(config_file):
+    os.environ['IBM_CREDENTIALS_FILE'] = config_file
+
+    # begin-common
+
+    global_tagging_service = GlobalTaggingV1.new_instance()
+
+    # end-common
+    assert global_tagging_service is not None
+
+    # Load the configuration
+    global config
+    config = read_external_sources(
+        GlobalTaggingV1.DEFAULT_SERVICE_NAME)
+
+    global resource_crn
+    resource_crn = config.get("RESOURCE_CRN")
+
+print('Setup complete.')
+
+needscredentials = pytest.mark.skipif(
+not os.path.exists(config_file), reason="External configuration not available, skipping..."
+)
+
+
+try:
+    # begin-attach_tag
+
+    resource_model = {
+        'resource_id': resource_crn
+    }
+
+    tag_results = global_tagging_service.attach_tag(
+        resources=[resource_model],
+        tag_names=['tag_test_1', 'tag_test_2']
+    ).get_result()
+
+    print(json.dumps(tag_results, indent=2))
+
+    # end-attach_tag
+
+except ApiException as e:
+    pytest.fail(str(e))
